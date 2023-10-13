56455648367506fc01ed3ee2d6abe2108689d35e
carivantelva <carlostello_25@hotmail.com>
2023-10-13 08:30:46 -0500

    hago cambios para mejorar aplicacion

--- carrito.js
+++ carrito.js
@@ -1,62 +1,192 @@
 
+    const carrito = document.querySelector('#carrito');
+    const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+    
+    const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+    const comprarCarritoBtn = document.querySelector('#comprarBtn');
 
-    // Datos del carrito )
-    const cart = [
-        { name: "Producto 1", price: 10.00, quantity: 1 },
-        { name: "Producto 2", price: 15.00, quantity: 1 },
-    ];
+    const listaArticulos = document.querySelector('#lista-articulos');
 
-    function updateCart() {
-        let cartTotal = 0;
-        const cartTable = document.getElementById("cart-table");
+    let articulosCarrito = [];
 
-        cart.forEach((product, index) => {
-            const quantityElement = document.getElementById(`quantity-${index}`);
-            const totalElement = document.getElementById(`total-${index}`);
 
-            quantityElement.textContent = product.quantity;
-            totalElement.textContent = `$${(product.price * product.quantity).toFixed(2)}`;
+    
 
-            cartTotal += product.price * product.quantity;
+    cargarEventListeners() 
+
+    function cargarEventListeners() {
+        // Cuando agregas una prenda presionando "Agregar al carrito"
+        listaArticulos.addEventListener('click',agregarArticulo);
+
+        //Elimina articulos del carrito
+        carrito.addEventListener('click', eliminarArticulo);  
+        
+        //muestra los articulos del local Storage
+        document.addEventListener('DOMContentLoaded', () => {
+            articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+            carritoHTML();
+
         });
 
-        document.getElementById("cart-total").textContent = `Total: $${cartTotal.toFixed(2)}`;
+        //vaciar el carrito
+        vaciarCarritoBtn.addEventListener('click', () => {
+            //console.log('vaciando carrito ....')
+            articulosCarrito = [];//aqui reseteamos el arreglo
+
+            limpiarHTML();//Eliminamos todo el HTML
+
+            localStorage.removeItem('carrito');
+        });
+
+
+        //comprar carrito
+       comprarCarritoBtn.addEventListener('click', () => {
+
+        
+           // console.log('comprando carrito ....')
+          
+
+           articulosCarrito = [];   
+           window.alert("su compra se esta confirmando");        
+
+            limpiarHTML();//Eliminamos todo el HTML
+
+            localStorage.removeItem('carrito');
+        });
+
+        
     }
 
-    function decrementQuantity(index) {
-        if (cart[index].quantity > 1) {
-            cart[index].quantity--;
-            updateCart();
+    // Funciones
+    function agregarArticulo(e) {
+
+        e.preventDefault(); //AQUI SE PREVIENE QUE SALTE HACIA ARRIBA
+
+        if(e.target.classList.contains('agregar-carrito')){
+            const articuloSeleccionado = e.target.parentElement.parentElement;
+            leerDatosArticulo(articuloSeleccionado);
         }
+        
     }
 
-    function incrementQuantity(index) {
-        cart[index].quantity++;
-        updateCart();
+    //elimina un articulo del carrito
+    function eliminarArticulo(e){
+        if(e.target.classList.contains('borrar-articulo')){
+
+       // console.log('desde eliminar articulo ....') para probar codigo eliminar articulo
+
+       const articuloId = e.target.getAttribute('data-id');
+
+       //Elimina dl arreglo de articulosCarrito por el data-id
+
+       articulosCarrito = articulosCarrito.filter( articulo => articulo.id !== articuloId);
+
+       carritoHTML();//interar sobre el carrito y mostrar su HTML
+
+       };
     }
-
-    function removeProduct(index) {
-        // Eliminar la fila de la tabla correspondiente al producto
-        const cartTable = document.getElementById("cart-table");
-        cartTable.deleteRow(index + 1); // +1 para compensar el encabezado de la tabla
-        cart.splice(index, 1); // Eliminar el producto del carrito
-        updateCart();
-    }
-
-    function checkout() {
-        // Aquí puedes implementar la lógica para finalizar la compra
-        alert("Compra realizada con éxito.");
-
-        location.reload()
-    }
-
-    /************************************************ */
-
     
 
+    //aqui lee el contenido del HTML al que le dimos click y extrae la informacion del articulo
+    function leerDatosArticulo(articulo){
+        //console.log(articulo);
 
-    /**************************************************** */
+        //creamos un objeto con el contenido del articulo actual
 
-    // Inicializar el carrito
-    updateCart();
+        const infoArticulo = {
+            imagen: articulo.querySelector('img').src,
+            titulo: articulo.querySelector('h4').textContent,
+            precio: articulo.querySelector('.precio span').textContent,
+            id:articulo.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+        /************************************************************************ */
+
+       
+
+        /************************************************************************** */
+
+
+
+        //revisa si un articulo ya existe en el carrito
+        const existe = articulosCarrito.some(articulo => articulo.id === infoArticulo.id);
+
+        //console.log(existe); //para verificar codigo false o true
+
+        if(existe){
+            //actualizamos la cantidad
+            const articulos = articulosCarrito.map(articulo => {
+                if (articulo.id === infoArticulo.id){
+                    articulo.cantidad++;
+                    return articulo; //este retorna el objeto actualizado
+                }else{
+                    return articulo; // retorna los objetos no duplicados
+                }
+            }) 
+
+            articulosCarrito = [...articulos];
+            
+        }else{articulosCarrito = [...articulosCarrito, infoArticulo];
+            //agregamos el articulo al carrito
+        }
+
+        console.log(infoArticulo) //para verificar la creaccion del objeto
+
+        //agrego elementos al arreglo de carrito
+       // articulosCarrito = [...articulosCarrito, infoArticulo]; //aqui tomo una copia del arreglo carrito
+       // console.log(articulosCarrito);
+
+        carritoHTML();
+    }
+
+    //muestra el carrito de compra en el HTML
+    function carritoHTML(){
+
+        //limpiar HTML
+        limpiarHTML();
+
+        //recorre el carrito y genera el html
+        articulosCarrito.forEach (articulo => {
+            //console.log(articulo);
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td> <img src="${articulo.imagen}" width="90" </td>
+            <td>${articulo.titulo}</td>
+            <td>${articulo.precio}</td>
+            <td>${articulo.cantidad}</td>
+            <td>
+            <a href="#" class="borrar-articulo" data-id="${articulo.id}"> x </a>
+            </td>
+            
+            `;
+            //agrega el html del carrito en el tbody
+            contenedorCarrito.appendChild(row);
+        });
+
+        //agregar el carrito al storage
+        sincronizarStorage();
+    }
+
+    function sincronizarStorage(){
+        localStorage.setItem('carrito', JSON.stringify(articulosCarrito));
+    }
+
+
+    //elimina los articulos del tbody
+    function limpiarHTML(){
+
+        //forma lenta de limpiar html       
+       // contenedorCarrito.innerHTML = '';
+       while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+       }
+    }
+
+        
+
+
     
+    
+
--- loguin.js
+++ loguin.js
@@ -1,46 +1,30 @@
 
+document.getElementById("olvidasteContrasena").addEventListener("click", function () {
+    alert("¡Olvidaste tu contraseña! Busca en tus apuntes para recuperarla porque esta herramienta aún no funciona. ☺☺☺");
+});
 
-// Función para validar campos de entrada
-function validateInput(inputElement, validacionForm, mensajeError) {
-    inputElement.addEventListener("input", function() {
-        if (validacionForm.test(inputElement.value)) {
-            inputElement.setCustomValidity("");
-        } else {
-            inputElement.setCustomValidity(mensajeError);
-        }
-    });
-}
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var password = document.getElementById("password").value;
 
-// Obtener elementos de entrada
-let emailInput = document.getElementById("email");
-let passwordInput = document.getElementById("password");
+    // Expresión regular para validar el formato de correo electrónico
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-
-
-// Definir expresiones regulares y mensajes de error
-let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-let emailErrorMessage = "Por favor, ingrese una dirección de correo electrónico válida.";
-
-let passwordPattern = /.{8,}/;
-let passwordErrorMessage = "La contraseña debe tener al menos 8 caracteres.";
-
-// Validar campos de entrada
-validateInput(emailInput, emailPattern, emailErrorMessage);
-validateInput(passwordInput, passwordPattern, passwordErrorMessage);
-
-/********************************* */
+    if (email === "" || password === "") {
+        alert("Por favor, completa todos los campos.");
+    }else if (!emailPattern.test(email)) {
+        alert("El formato del correo electrónico no es válido.");
+    
+    }else if (password.length < 8) {
+        alert("La contraseña debe tener al menos 8 caracteres.");
+    } else {
+        window.location.href = "homeCarrito.html";
+    }
+});
 
 
-const apellidosInput = document.getElementById("apellidos");
-    const apellidosError = document.getElementById("apellidos-error");
 
-    apellidosInput.addEventListener("input", function () {
-        const value = apellidosInput.value.trim();
-        const regex = /^[A-Za-z\s]+$/; // Expresión regular para letras y espacios
 
-        if (regex.test(value)) {
-            apellidosError.textContent = ""; // Campo válido
-        } else {
-            apellidosError.textContent = "Solo se permiten letras y espacios en los apellidos";
-        }
-    });
+
